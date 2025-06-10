39c7ec72047480a9fd34e9aaec54b9ab41d15a7d
Segers Ian <contact@segersian.com>
2025-06-10 11:54:25 +0200

    Add discusssion

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -60,6 +60,7 @@ module.exports = {
 
   plugins: [
     ["@vuepress/plugin-google-analytics", { ga: "UA-76745643-3" }],
+    ['plausible', { domain: 'segersian.com' }],
     ["@vuepress/medium-zoom"],
     [
       "@vuepress/blog",
